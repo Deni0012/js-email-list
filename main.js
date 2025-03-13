22306701bd68f5daf55b62c800ad7b5f089f8f51
Deni0012 <deni.dg00@icloud.com>
2025-03-13 16:39:48 +0100

    feat: collegamenti delle strutture e link

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-console.log('Ciao Denise')
+const emailList = [];
+const urlAPI = "https://flynn.boolean.careers/exercises/api/random/mail";
+const parentElement = document.querySelector(".list-container")
+console.log(parentElement);
