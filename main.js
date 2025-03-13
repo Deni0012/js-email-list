bd22604ea88e37c2aee558ba34bb655ffe53015b
Deni0012 <deni.dg00@icloud.com>
2025-03-13 16:44:15 +0100

    feat: axios API

--- main.js
+++ main.js
@@ -2,3 +2,16 @@ const emailList = [];
 const urlAPI = "https://flynn.boolean.careers/exercises/api/random/mail";
 const parentElement = document.querySelector(".list-container")
 console.log(parentElement);
+
+for (let i = 0; i < 10; i++) {
+    axios.get(urlAPI)
+        .then(response => {
+            emailList[i] = response.data.response;
+        })
+        .catch(error => console.error(error));
+}
+
+console.log(emailList)
+
+
+renderHTML(parentElement, emailList)
